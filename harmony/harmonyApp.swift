56680e3b5c15241c5c585bd8b8d59e64122d059e
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-07-02 23:16:39 -0400

    Initial Commit

---
+++ harmony/harmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  harmonyApp.swift
+//  harmony
+//
+//  Created by Samuel Valencia on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct harmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
