ef8c6f406fc55cdf1e6abfd53e159eeca0904993
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-12-10 08:08:20 -0500

    Start implementing recently played items

--- vectis/Views/HomeView.swift
+++ vectis/Views/HomeView.swift
@@ -6,10 +6,62 @@
 //
 
 import SwiftUI
+import MusicKit
 
 struct HomeView: View {
+    @State var items: [RecentlyPlayedMusicItem] = []
+    @State private var isLoading = false
+    
     var body: some View {
-        Text("Home View")
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Home")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 15)
+                    
+                    if isLoading {
+                        ProgressView().padding()
+                    } else {
+                        LazyVStack(spacing: 10) {
+                            ForEach(Array(items.enumerated()), id: \.element.id) { index, item in
+                                switch item {
+                                case .album(let album):
+                                    Text(album.title)
+                                case .playlist(let playlist):
+                                    Text(playlist.name)
+                                default:
+                                    EmptyView()
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.leading, 15)
+            }
+        }.task {
+            if items.isEmpty {
+                await loadRecent()
+            }
+        }
+    }
+    
+    @MainActor
+    func loadRecent() async {
+        isLoading = true
+        defer { isLoading = false }
+        
+        do {
+            async let recentFetch = MusicRecentlyPlayedRequest<RecentlyPlayedMusicItem>().response()
+            let recentResponse = try await recentFetch
+            
+            print("Recent Items: \(recentResponse.items)")
+            items = Array(recentResponse.items)
+            
+        } catch {
+            print("Failed to load recent playlists: \(error)")
+        }
     }
 }
 
